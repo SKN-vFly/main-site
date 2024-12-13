5d48c9b0007f6e9243d2e53a31cc79329918aa52
Libi Rajzer <michal.rajzer03@gmail.com>
2024-12-14 00:16:44 +0100

    Small visual improvements

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,30 +8,47 @@ const Navbar = () => {
   const t = useTranslations("Components.Navbar");
   return (
     <>
-      <nav className="p-4 bg-black border-b border-white whitespace-nowrap fixed w-full">
-        <ul className="flex justify-end space-x-4 ">
-          <li className={"p-2"}>
+      <nav className="p-1 bg-black border-b border-white whitespace-nowrap fixed w-full">
+        <ul className="flex">
+          <li className={"p-1 mx-1"}>
             <Link href="/">logo</Link>
           </li>
-          <li>
+          <li className="mx-1">
             <LangSwitcher />
           </li>
-          <li className={" w-full"}></li>
-          <li className={"border border-white p-2 text-center rounded-lg"}>
+          <li className={"w-full"}></li>
+          <li
+            className={
+              "border border-white p-1 mx-1 text-center rounded-lg my-auto"
+            }
+          >
             <Link href="/">{t("home")}</Link>
           </li>
-          <li className={"border border-white p-2 text-center rounded-lg"}>
+          <li
+            className={
+              "border border-white p-1 mx-1 text-center rounded-lg my-auto"
+            }
+          >
             <Link href="/projects">{t("projects")}</Link>
           </li>
-          <li className={"border border-white p-2 text-center rounded-lg"}>
+          <li
+            className={
+              "border border-white p-1 mx-1 text-center rounded-lg my-auto"
+            }
+          >
             <Link href="/contact">{t("contact")}</Link>
           </li>
-          <li className={"border border-white p-2 text-center rounded-lg"}>
+          <li
+            className={
+              "border border-white p-1 mx-1 text-center rounded-lg my-auto"
+            }
+          >
             <Link href="/join-us">{t("join")}</Link>
           </li>
         </ul>
       </nav>
-      <div className="p-6">hidden text</div>
+      <div className="p-2">.</div>
+      {/* Hidden text to make floating navbar align nicely */}
     </>
   );
 };
@@ -49,7 +66,7 @@ function LangSwitcher() {
       onMouseLeave={() => setShowLangsHovered(false)}
     >
       <button
-        className={`border border-white p-2 text-center ${
+        className={`border border-white p-1 text-center ${
           showLangsClicked || showLangsHovered
             ? "rounded-t-lg rounded-b-0"
             : "rounded-lg"
@@ -59,7 +76,7 @@ function LangSwitcher() {
         {t("lang")}
       </button>
       <div
-        className={`absolute top-10 -translate-x-1/3 bg-black border border-white rounded-md p-2 flex flex-col  ${
+        className={`absolute top-8 -translate-x-1/3 bg-black border border-white rounded-md p-2 flex flex-col  ${
           showLangsClicked || showLangsHovered ? "" : "hidden"
         }`}
       >
