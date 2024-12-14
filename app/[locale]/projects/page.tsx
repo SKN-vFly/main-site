af1aae8b25fb0e0e0223af42c3315829ea5d377a
Libi Rajzer <michal.rajzer03@gmail.com>
2024-12-14 02:37:49 +0100

    Very WIP project display

--- app/[locale]/projects/page.tsx
+++ app/[locale]/projects/page.tsx
@@ -1,13 +1,85 @@
 import { useTranslations } from "next-intl";
 import { localeArray } from "@/i18n/routing";
+import { ProjectDisplay } from "@/components/ProjectDisplayWrapper";
+const projects = [
+  {
+    title: "Project 1",
+    description: "Description 1",
+    imgSrc: "/placeholder.jpg",
+    link: "/projects/project1",
+  },
+  {
+    title: "Project 2",
+    description: "Description 2",
+    imgSrc: "/placeholder.jpg",
+    link: "/projects/project2",
+  },
+  {
+    title: "Project 3",
+    description: "Description 3",
+    imgSrc: "/placeholder.jpg",
+    link: "/projects/project3",
+  },
+  {
+    title: "Project 4",
+    description: "Description 4",
+    imgSrc: "/placeholder.jpg",
+    link: "/projects/project4",
+  },
+  {
+    title: "Project 5",
+    description: "Description 5",
+    imgSrc: "/placeholder.jpg",
+    link: "/projects/project5",
+  },
+  {
+    title: "Project 6",
+    description: "Description 6",
+    imgSrc: "/placeholder.jpg",
+    link: "/projects/project6",
+  },
+  {
+    title: "Project 7",
+    description: "Description 7",
+    imgSrc: "/placeholder.jpg",
+    link: "/projects/project7",
+  },
+  {
+    title: "Project 8",
+    description: "Description 8",
+    imgSrc: "/placeholder.jpg",
+    link: "/projects/project8",
+  },
+  {
+    title: "Project 9",
+    description: "Description 9",
+    imgSrc: "/placeholder.jpg",
+    link: "/projects/project9",
+  },
+  {
+    title: "Project 10",
+    description: "Description 10",
+    imgSrc: "/placeholder.jpg",
+    link: "/projects/project10",
+  },
+  {
+    title: "Project 11",
+    description: "Description 11",
+    imgSrc: "/placeholder.jpg",
+    link: "/projects/project11",
+  },
+];
 
 export default function Projects() {
   const t = useTranslations("ProjectsPage");
+
   return (
-    <div>
-      <h1>{t("title")}</h1>
-      <p>{t("body")}</p>
-    </div>
+    <>
+      <div className="my-3">
+        <h1 className="text-3xl text-center">{t("title")}</h1>
+      </div>
+      <ProjectDisplay projects={projects} />
+    </>
   );
 }
 
---
+++ components/ProjectComponent.tsx
@@ -0,0 +1,83 @@
+"use client";
+import { useEffect, useState, useContext } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { ProjectClickedContext } from "./ProjectDisplayWrapper";
+
+export function ProjectComponent(props: {
+  title: string;
+  description: string;
+  imgSrc: string;
+  link: string;
+}) {
+  const [hover, setHover] = useState(false);
+  const [clicked, setClicked] = useState(false);
+  const { title, description, imgSrc, link } = props;
+  const { allowInteraction, setAllowInteraction } = useContext(
+    ProjectClickedContext
+  );
+
+  useEffect(() => {
+    setAllowInteraction(!clicked);
+  }, [clicked, setAllowInteraction]);
+  console.log("allowInteraction", allowInteraction);
+  return (
+    <>
+      {!clicked ? (
+        <div
+          className={`border border-white rounded-2xl p-2 transition-all ${
+            hover ? "m-2" : "m-5"
+          }`}
+          onMouseEnter={() => allowInteraction && setHover(true)}
+          onMouseLeave={() => allowInteraction && setHover(false)}
+          onClick={() => allowInteraction && setClicked(true)}
+        >
+          <div className="relative aspect-square">
+            <Image
+              src={imgSrc}
+              alt={title}
+              fill={true}
+              className="rounded-t-2xl"
+            />
+          </div>
+          <h3 className="font-bold text-center">{title}</h3>
+        </div>
+      ) : (
+        <>
+          <div
+            className="border border-white rounded-2xl p-2 opacity-0"
+            onClick={() => setClicked(true)}
+          >
+            <div className="relative aspect-square">
+              <Image
+                src={imgSrc}
+                alt={title}
+                fill={true}
+                className="rounded-t-2xl"
+              />
+            </div>
+            <h3>{title}</h3>
+          </div>
+          <div
+            className="bg-black border border-white rounded-2xl p-2 absolute z-[100] translate-x-1/4 w-[66%]"
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+            onClick={() => setClicked(false)}
+          >
+            <h3 className="">{title}</h3>
+            <div className="relative aspect-square ">
+              <Image
+                src={imgSrc}
+                alt={title}
+                fill={true}
+                className="rounded-t-2xl "
+              />
+            </div>
+            <p className="">{description}</p>
+            <Link href={link}>Learn more</Link>
+          </div>
+        </>
+      )}
+    </>
+  );
+}
