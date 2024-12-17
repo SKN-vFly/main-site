ca7ee9313b81d905477bf4872dcac2d06af5738a
Libi Rajzer <michal.rajzer03@gmail.com>
2024-12-17 22:24:28 +0100

    Refactored contact/join-us

--- app/[locale]/contact/page.tsx
+++ app/[locale]/contact/page.tsx
@@ -1,12 +1,47 @@
 import { useTranslations } from "next-intl";
+import { Link } from "@/i18n/routing";
 // import { localeArray } from "@/i18n/routing";
 
 export default function Contact() {
-  const t = useTranslations("Contact");
+  const t = useTranslations("ContactPage");
   return (
     <div>
-      <h1>{t("title")}</h1>
-      <p>{t("body")}</p>
+      <h1 className="my-5 text-5xl">{t("joinHeader")}</h1>
+      <h2 className="text-3xl text-center">{t("leaders")}</h2>
+      <div className="grid gap-5 grid-cols-2 w-2/3 mx-auto my-10">
+        <div className="w-full aspect-[3/4] border border-black dark:border-white rounded-3xl text-center">
+          huh
+        </div>
+        <div className="w-full aspect-[3/4]  border border-black dark:border-white rounded-3xl text-center">
+          huh2
+        </div>
+      </div>
+      <h2 className="text-3xl text-center">{t("sectLeads")}</h2>
+
+      <div className="grid gap-5 grid-cols-4 w-full my-10">
+        <div className="w-full aspect-[3/4] border border-black dark:border-white rounded-3xl text-center">
+          huh
+        </div>
+        <div className="w-full aspect-[3/4]  border border-black dark:border-white rounded-3xl text-center">
+          huh2
+        </div>
+        <div className="w-full aspect-[3/4]  border border-black dark:border-white rounded-3xl text-center">
+          huh3
+        </div>
+        <div className="w-full aspect-[3/4]  border border-black dark:border-white rounded-3xl text-center">
+          huh4
+        </div>
+      </div>
+
+      <div className="my-10 flex">
+        <Link
+          className="px-5 py-2 border border-black dark:border-white text-xl mx-auto rounded-xl"
+          href="https://discord.gg/57huavgd"
+          target="_blank"
+        >
+          {t("join")}
+        </Link>
+      </div>
     </div>
   );
 }
