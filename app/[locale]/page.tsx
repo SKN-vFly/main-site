749a0051f51373d5af10084041141f5c2d694ad7
Libi Rajzer <michal.rajzer03@gmail.com>
2024-12-17 07:23:59 +0100

    Added metadata + footer impl + small visual changes

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -5,10 +5,13 @@ import { localeArray } from "@/i18n/routing";
 export default function Landing() {
   const t = useTranslations("MainPage");
   return (
-    <div className="w-full min-h-[600vh]">
-      <MediaHeader text={t("title")} />
-      <div className="p-4 mx-auto w-[60%]">
-        <h2>{t("subtitle")}</h2>
+    <div className="w-full ">
+      <div className="w-full h-[96vh]">
+        {/* Niech ktos obliczy ile wysokosci zeby bylo do spodu strony a nie nizej */}
+        <MediaHeader text={t("title")} />
+        <div className="p-4 mx-auto w-[60%]">
+          <h2>{t("subtitle")}</h2>
+        </div>
       </div>
     </div>
   );
