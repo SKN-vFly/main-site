980883fdf91cd8e9639d571820dad7a62e950c65
Michał Czyż <mike@c2yz.com>
2025-07-18 12:18:25 +0200

    feat: cms integration

--- old/next.config.ts
+++ old/next.config.ts
@@ -1,3 +1,4 @@
+import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
  
@@ -8,4 +9,4 @@ const nextConfig: NextConfig = {
   // No need for turbopack config as it's handled by CLI flag
 };
 
-export default withNextIntl(nextConfig);
+export default withPayload(withNextIntl(nextConfig));
